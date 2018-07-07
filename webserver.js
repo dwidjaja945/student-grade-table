2f1856925d59b5c801e9e5af34691651ede6b5f7
Dylan Widjaja <dwidjaja945@gmail.com>
2018-07-06 18:53:41 -0700

    bundled client. Attempting Deployment

--- webserver.js
+++ webserver.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const path = require('path');
 const webserver = express();
 const { credentials } = require('./config/mysqlCredentials');
 const database = mysql.createConnection( credentials );
@@ -11,9 +12,9 @@ webserver.use(bodyParser.json());
 database.connect( (error) => {
     if( error ) throw error;
     console.log("database connection successful");
-} );
+});
 
-webserver.use(express.static( __dirname + "/client" + "/public" ));
+webserver.use(express.static( __dirname + "/client" + "/build" ));
 
 // ====================================
 // ======= Endpoints start here =======
@@ -23,10 +24,14 @@ require('./routes')( webserver , mysql , database );
 
 webserver.get( '/test' , ( req , res ) => {
     res.sendFile("/Users/dylanwidjaja/Desktop/projects/student-grade-table/test.html");
-})
+});
+
+// webserver.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
 
 
 webserver.listen( 9000 , () => {
     console.log("webserver listening on port 9000");
-} )
+});
 
